ab00632a812e76a989f5a27299d5040d93170643
sithu <sithuhotline@gmail.com>
2026-06-11 14:43:23 +0630

    user modle

--- models/user_model.js
+++ models/user_model.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const userSchema = new Schema ({
+const UserSchema = new Schema({
 
-    name:String,
-    phone:String,
-    password:String,
-    type:String,
-    unit:Schema.Types.Double,
-    created:Date,
+    name: { type: String, required: true, unique: true },
+    phone: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    type: { type: String, enum: ['BUYER', 'SELLER'], default: 'BUYER' },
+    unit: { type: Schema.Types.Int32, default: 0 },
+    created: { type: Date, default: Date.now }
 
-})
+});
+
+const User = mongoose.model('users', UserSchema);
+
+module.exports = User;
